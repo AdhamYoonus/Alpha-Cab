6dd8313d174876c7a10e57ae5142832800e65d70
S1802035 <118211442+S1802035@users.noreply.github.com>
2023-02-17 14:47:20 +0500

    Packages and files classes imported

--- src/java/com/alphacab/controllers/UpdateCustomerServlet.java
+++ src/java/com/alphacab/controllers/UpdateCustomerServlet.java
@@ -1,5 +1,15 @@
 package com.alphacab.controllers;
 
+import com.alphacab.dao.CustomerDAO;
+import com.alphacab.models.Customer;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class UpdateCustomerServlet {
     
 }
