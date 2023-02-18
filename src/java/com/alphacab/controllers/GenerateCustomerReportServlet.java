3f837088b1562dad77b9ba1cdb30ecd4d74275b4
Shunan <s2001349@students.villacollege.edu.mv>
2023-02-18 19:41:14 +0500

    Add customer report generating method

--- src/java/com/alphacab/controllers/GenerateCustomerReportServlet.java
+++ src/java/com/alphacab/controllers/GenerateCustomerReportServlet.java
@@ -1,5 +1,49 @@
 package com.alphacab.controllers;
 
-public class GenerateCustomerReportServlet {
-    
+import com.alphacab.dao.BookingDAO;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.alphacab.models.Booking;
+
+@WebServlet(name = "GenerateCustomerReportServlet", urlPatterns = {"/admin/customer-report"})
+public class GenerateCustomerReportServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        String action = request.getParameter("action");
+        if(action == null)
+            action = "report";
+        if(action.equals("reportOnDate"))
+        {
+            BookingDAO dao = new BookingDAO();
+            Date date = (Date) request.getSession().getAttribute("date");
+            List<Booking> list = dao.getAllBookingsCompletedOnDate(date);
+            request.setAttribute("list", list);
+            request.getRequestDispatcher("/WEB-INF/jsp/view/customer/customerReport.jsp").forward(request, response);
+        }
+        if(action.equals("report"))
+        {
+            Date date = new Date(System.currentTimeMillis());
+            
+            request.getSession().setAttribute("date", date);
+            List<Booking> list = new BookingDAO().getAllBookingsCompletedOnDate(date);
+            request.setAttribute("list", list);
+            request.getRequestDispatcher("/WEB-INF/jsp/view/customer/customerReport.jsp").forward(request, response);
+        }
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
 }
