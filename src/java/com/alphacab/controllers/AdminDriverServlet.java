99614aa6393460677d08f9c5a4dfa0fbbd972cae
Shunan <s2001349@students.villacollege.edu.mv>
2023-02-18 19:01:24 +0500

    Implement doGet register and update APIs

--- src/java/com/alphacab/controllers/AdminDriverServlet.java
+++ src/java/com/alphacab/controllers/AdminDriverServlet.java
@@ -1,5 +1,42 @@
 package com.alphacab.controllers;
 
-public class AdminDriverServlet {
-    
+import com.alphacab.dao.DriverDAO;
+import com.alphacab.models.Driver;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "AdminServlet", urlPatterns = {"/admin/driver"})
+public class AdminDriverServlet extends HttpServlet 
+{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        String action = request.getParameter("action");
+        
+        switch(action)
+        {
+            case "register":
+                forwardRequest("driver/register", request, response);
+                break;
+            case "update":
+                String username = request.getParameter("username");
+                Driver driver = new DriverDAO().getDriverWithUsername(username);
+                request.setAttribute("driver", driver);
+                forwardRequest("driver/update", request, response);
+                break;
+
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
 }
